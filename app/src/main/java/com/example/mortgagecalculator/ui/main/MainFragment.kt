d5f984e134dcc8d5cbd1acab37d190bbb416aded
Jerry Izaguirre <Gerardoiza94@gmail.com>
2020-05-16 22:27:58 -0500

    comit

--- app/src/main/java/com/example/mortgagecalculator/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mortgagecalculator/ui/main/MainFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.mortgagecalculator.databinding.MainFragmentBinding
-import kotlinx.android.synthetic.main.main_fragment.*
 import kotlin.math.pow
 
 
@@ -19,57 +17,37 @@ class MainFragment : Fragment() {
     }
 
 
-
     private lateinit var viewModel: MainViewModel
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val binding = MainFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-
-        val MyInput = MortgageInput(
-            "10000","5","30","1")
-
-        val initialPrincipal = MyInput.inputLoan.toDouble()
-        val loanYears = MyInput.inputYears.toDouble()
-        val interest = MyInput.inputInterest.toDouble()
-        val downPayment = MyInput.inputDownPayment.toDouble()
+        val initialPrincipal = binding.loanAmount.text.toString().toDouble()
+        val loanYears = binding.loanTerm.text.toString().toDouble()
+        val interest = binding.interestAmount.text.toString().toDouble()
+        val downPayment = binding.moneyDown.text.toString().toDouble()
 
 
         fun quickMaths(): Double {
 
-
-
             val nOfPayments = loanYears.times(12)
             val calculatedInt = interest.div((100 * 12))
             val calExponent = (calculatedInt.plus(1).let {
-                nOfPayments.let { it1 ->
-                    it.pow(it1)
-                }
-            })
-
-            return ((calExponent.let {
-                calculatedInt.times(
-                    it
-                )
-            }).div(calExponent - 1)).let {
+                nOfPayments.let { it1 -> it.pow(it1) } })
+            return ((calExponent.let { calculatedInt.times(it) }).div(calExponent - 1)).let {
                 (downPayment.let { initialPrincipal.minus(it) }).times(
                     it
                 )
             }
-
         }
 
-        val tehe = quickMaths()
 
         binding.btn.setOnClickListener{
-            binding.textView.text = tehe.toString()
+            binding.textView.text = quickMaths().toString()
 
         }
-
-
         return binding.root
     }
 
--- app/src/main/java/com/example/mortgagecalculator/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/mortgagecalculator/ui/main/MainViewModel.kt
@@ -1,17 +1,12 @@
 package com.example.mortgagecalculator.ui.main
 
-import android.util.Log
+import androidx.databinding.InverseMethod
+import androidx.databinding.ObservableArrayMap
 import androidx.lifecycle.ViewModel
+
 
 class MainViewModel : ViewModel() {
 
-    fun onTextChanged(
-        s: CharSequence,
-        start: String,
-        before: String,
-        count: String
-    ) {
-        Log.w("tag", "onTextChanged $s")
-    }
+    val inputPrice = ObservableArrayMap<Any, Int>()
 
 }
